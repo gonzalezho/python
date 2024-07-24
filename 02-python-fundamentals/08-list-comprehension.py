a336fd7b20d9ad4d2f30cc56f4fe2cf897f25a2c
gonzalezho <hugo.orlando2050@gmail.com>
2024-07-24 19:15:41 +0000

    python fundamentals

--- 02-python-fundamentals/08-list-comprehension.py
+++ 02-python-fundamentals/08-list-comprehension.py
@@ -15,6 +15,32 @@ nested_list = [[1, 2, 3], [4, 5], [6, 7, 8]]
 flattened = [item for sublist in nested_list for item in sublist]
 print(flattened)  # Output: [1, 2, 3, 4, 5, 6, 7, 8]
 
+ages = [22, 35, 27, 21, 20]
+odds = [n for n in ages if n % 2 == 1]
+
+# -- with strings --
+
+friends = ["Rolf", "ruth", "charlie", "Jen"]
+guests = ["jose", "Bob", "Rolf", "Charlie", "michael"]
+
+friends_lower = [f.lower() for f in friends]
+
+present_friends = [
+    name.capitalize() for name in guests if name.lower() in friends_lower
+]
+
+# -- nested list comprehensions --
+# Don't do this, because it's almost completely unreadable.
+# Splitting things out into variables is better.
+
+friends = ["Rolf", "ruth", "charlie", "Jen"]
+guests = ["jose", "Bob", "Rolf", "Charlie", "michael"]
+
+present_friends = [
+    name.capitalize() for name in guests if name.lower() in [f.lower() for f in friends]
+]
+
+
 # Apply a function to each element in a list
 def square(x):
     return x**2
