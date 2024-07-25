62506b9bef78aa0a343d74eccf4d67c806543f97
gonzalezho <hugo.orlando2050@gmail.com>
2024-07-25 15:21:53 +0000

    dunder methods added

--- 03-oop/03-dunder-methods.py
+++ 03-oop/03-dunder-methods.py
@@ -11,11 +11,21 @@ class Garage:
     def __getitem__(self, index: int) -> str:
         return self.cars[index]
     
+    def __repr__(self) -> str:
+        return f"<Garage {self.cars}>"
+    
+    def __str__(self) -> str:
+        return f"Garage with {len(self)} cars"
+    
 
 if __name__ == "__main__":
     ford = Garage()
     ford.cars.append("Fiesta")
     ford.cars.append("Focus")
+    print(ford)
 
-    res = Garage.__getitem__(ford, 1)
-    print(res)
+    # g1 = Garage.__getitem__(ford, 1)
+    # print(g1)
+
+    # g2 = Garage.__repr__(ford)
+    # print(g2)
